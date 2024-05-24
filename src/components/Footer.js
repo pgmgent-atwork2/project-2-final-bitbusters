f20bc297935b17d511c2ae41c7b248ca6b441000
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2024-05-24 13:10:32 +0200

    24 may

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer>
+            <div className="nav-content">
+                <h5>De Bijloke</h5>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/steps">Steps</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
 }
